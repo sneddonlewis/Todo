19812570b8a26769bbdd34eaee83aea058e995fa
Lewis Sneddon <lewis@lewiscodes.io>
2023-03-25 18:19:44 +0000

    CRUD ops from web api controller

--- Todo.Api/Controllers/TodoItemController.cs
+++ Todo.Api/Controllers/TodoItemController.cs
@@ -1,5 +1,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Todo.Application.Features.TodoItems.Commands.CreateTodoItem;
+using Todo.Application.Features.TodoItems.Commands.DeleteTodoItem;
+using Todo.Application.Features.TodoItems.Commands.UpdateTodoItem;
 using Todo.Application.Features.TodoItems.Queries.GetTodoItemsList;
 
 namespace Todo.Api.Controllers;
@@ -19,7 +22,36 @@ public class TodoItemController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<List<TodoItemListVm>>> GetAllTodos()
     {
-        var dtos = await _mediator.Send(new GetTodoItemsListQuery());
-        return Ok(dtos);
+        var dtoList = await _mediator.Send(new GetTodoItemsListQuery());
+        return Ok(dtoList);
+    }
+
+    [HttpPost(Name = "AddTodo")]
+    public async Task<ActionResult<Guid>> Create(
+        [FromBody] CreateTodoItemCommand request)
+    {
+        var response = await _mediator.Send(request);
+        return Ok(response);
+    }
+    
+    [HttpPut(Name = "UpdateTodo")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Update([FromBody] UpdateTodoItemCommand request)
+    {
+        await _mediator.Send(request);
+        return NoContent();
+    }
+    
+    [HttpDelete("{id}", Name = "DeleteTodo")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var deleteCommand = new DeleteTodoItemCommand { TodoItemId = id };
+        await _mediator.Send(deleteCommand);
+        return NoContent();
     }
 }
