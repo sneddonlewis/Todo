f66e62871bc66f6a48a890a3107d76ab72f0467c
Lewis Sneddon <lewis@lewiscodes.io>
2023-03-25 15:52:58 +0000

    Added Fluent Validation

---
+++ Todo.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+namespace Todo.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key)
+        : base($"{name} ({key}) is not found")
+    {
+        
+    }
+}
--- Todo.Application/Features/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandHandler.cs
+++ Todo.Application/Features/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandHandler.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using FluentValidation.Results;
 using MediatR;
 using Todo.Application.Contracts.Persistence;
+using Todo.Application.Exceptions;
 using Todo.Domain.Entities;
 
 namespace Todo.Application.Features.TodoItems.Commands.CreateTodoItem;
@@ -19,6 +21,13 @@ public class CreateTodoItemCommandHandler:
 
     public async Task<Guid> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
     {
+        var validator = new CreateTodoItemCommandValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult);
+        }
+        
         var todo = _mapper.Map<TodoItem>(request);
         todo = await _todoItemRepository.AddAsync(todo);
         return todo.TodoItemId;
